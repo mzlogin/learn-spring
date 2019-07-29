90a913372073bb44c7260dd19e9bed053fc4d5b2
Zhuang Ma <chumpma@gmail.com>
2019-07-29 20:18:16 +0800

    新增 getting start 工程
    ref https://spring.io/guides/gs/rest-service/

---
+++ getting-start/src/test/java/org/mazhuang/getting/start/GreetingControllerTests.java
@@ -0,0 +1,36 @@
+package org.mazhuang.getting.start;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class GreetingControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, World!"));
+    }
+
+    @Test
+    public void paramGreetingShouldReturnTailoreMessage() throws Exception {
+        this.mockMvc.perform(get("/greeting").param("name", "mzlogin"))
+                .andDo(print()).andExpect(status().isOk())
+                .andExpect(jsonPath("$.content").value("Hello, mzlogin!"));
+    }
+}
