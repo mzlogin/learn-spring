14231b39e67a916c6c394b725d140e7688669eb7
Zhuang Ma <chumpma@gmail.com>
2019-12-18 21:16:34 +0800

    add project serving web content

---
+++ web-content-test/src/main/java/org/mazhuang/webcontenttest/GreetingController.java
@@ -0,0 +1,20 @@
+package org.mazhuang.webcontenttest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * author: mazhuang
+ * date: 2019/12/18
+ */
+@Controller
+public class GreetingController {
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
---
+++ web-content-test/src/main/java/org/mazhuang/webcontenttest/WebContentTestApplication.java
@@ -0,0 +1,13 @@
+package org.mazhuang.webcontenttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebContentTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebContentTestApplication.class, args);
+    }
+
+}
