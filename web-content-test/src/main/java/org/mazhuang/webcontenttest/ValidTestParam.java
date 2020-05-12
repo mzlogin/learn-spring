9fedf73d2caedf82bc6ceea87e6e053aa4a822b8
Zhuang Ma <chumpma@gmail.com>
2020-05-12 19:55:52 +0800

    update valid demo

--- web-content-test/src/main/java/org/mazhuang/webcontenttest/ValidTestParam.java
+++ web-content-test/src/main/java/org/mazhuang/webcontenttest/ValidTestParam.java
@@ -5,6 +5,7 @@ import org.springframework.lang.Nullable;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 /**
@@ -24,13 +25,23 @@ public class ValidTestParam {
     private ParamProperties properties;
 
     /**
-     * 1。 propertiesList 可以为 null
+     * 1. propertiesList 可以为 null
      * 2. propertiesList 可以为 empty
      * 3. 否则 propertiesList 里的元素的属性值会被校验
      */
     @Nullable
     @Valid
     private List<ParamProperties> propertiesList;
+
+    /**
+     * 1. paramPropertiesList 不可以为 null
+     * 2. paramPropertiesList 不可以为 empty
+     * 3. paramPropertiesList 里的元素的属性值会被校验
+     * 4. 不加 @Valid 则里面的元素的属性值不会被校验
+     */
+    @NotEmpty
+    @Valid
+    private List<ParamProperties> paramPropertiesList;
 
     @Data
     public static class ParamProperties {
