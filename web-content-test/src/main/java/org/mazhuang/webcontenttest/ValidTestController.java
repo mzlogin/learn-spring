0d3ab979514e1ceec3be60ea02eb810507918b04
Zhuang Ma <chumpma@gmail.com>
2020-05-23 17:36:41 +0800

    update validate demo

--- web-content-test/src/main/java/org/mazhuang/webcontenttest/ValidTestController.java
+++ web-content-test/src/main/java/org/mazhuang/webcontenttest/ValidTestController.java
@@ -1,5 +1,6 @@
 package org.mazhuang.webcontenttest;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +17,17 @@ public class ValidTestController {
     @PostMapping("/valid/test")
     public String validTest(@RequestBody @Valid ValidTestParam param) {
         return "ok";
+    }
+
+    @GetMapping("/valid/testManualInvoke")
+    public String validTestManualInvoke() {
+        ValidTestParam.ParamProperties param = new ValidTestParam.ParamProperties();
+        // 这样调用无效，@Valid 不会起作用
+        manualInvokeTest(param);
+        return "ok";
+    }
+
+    private void manualInvokeTest(@Valid ValidTestParam.ParamProperties param) {
+
     }
 }
