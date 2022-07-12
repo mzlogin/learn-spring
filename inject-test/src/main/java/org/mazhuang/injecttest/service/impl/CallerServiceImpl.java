936e6037c57f4d6b78303a0d97f88c5e423ca945
Zhuang Ma <chumpma@gmail.com>
2022-07-12 15:43:35 +0800

    update

--- inject-test/src/main/java/org/mazhuang/injecttest/service/impl/CallerServiceImpl.java
+++ inject-test/src/main/java/org/mazhuang/injecttest/service/impl/CallerServiceImpl.java
@@ -1,5 +1,6 @@
 package org.mazhuang.injecttest.service.impl;
 
+import jdk.nashorn.internal.codegen.CompilerConstants;
 import org.mazhuang.injecttest.service.CalleeService;
 import org.mazhuang.injecttest.service.CallerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -85,25 +86,31 @@ public class CallerServiceImpl implements CallerService {
 //    @Resource(name = "callee3ServiceImpl", type = Callee2ServiceImpl.class)
 //    private CalleeService calleeService;
 
-    // Case 12: Autowired + 匹配 Bean name 的变量名
+    // Case 12: Resource + 注掉 CalleeService 的所有实现
+    // 运行结果：异常
+    // Exception encountered during context initialization - cancelling refresh attempt: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'callerServiceImpl': Injection of resource dependencies failed; nested exception is org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'org.mazhuang.injecttest.service.CalleeService' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {@javax.annotation.Resource(shareable=true, lookup=, name=, description=, authenticationType=CONTAINER, type=class java.lang.Object, mappedName=)}
+//    @Resource
+//    private CalleeService calleeService;
+
+    // Case 13: Autowired + 匹配 Bean name 的变量名
     // 运行结果：正常
 //    @Autowired
 //    private CalleeService callee1ServiceImpl;
 
-    // Case 13: Autowired + 不匹配 Bean name 的变量名
+    // Case 14: Autowired + 不匹配 Bean name 的变量名
     // 运行结果：异常
     // 此时将 Callee1ServiceImpl 和 Callee2ServiceImpl 注释掉其中一个就运行正常了。说明 Autowired 是优先按变量名寻找 Bean，如果找不到再按类型搜索。
     // Exception encountered during context initialization - cancelling refresh attempt: org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'callerServiceImpl': Unsatisfied dependency expressed through field 'calleeService'; nested exception is org.springframework.beans.factory.NoUniqueBeanDefinitionException: No qualifying bean of type 'org.mazhuang.injecttest.service.CalleeService' available: expected single matching bean but found 2: callee1ServiceImpl,callee2ServiceImpl
 //    @Autowired
 //    private CalleeService calleeService;
 
-    // Case 14: Autowired + 匹配 Bean name 的变量名 + Qualifier 匹配同一个 Bean name 的变量名
+    // Case 15: Autowired + 匹配 Bean name 的变量名 + Qualifier 匹配同一个 Bean name 的变量名
     // 运行结果：正常
 //    @Autowired
 //    @Qualifier("callee1ServiceImpl")
 //    private CalleeService callee1ServiceImpl;
 
-    // Case 15: Autowired + 匹配 Bean name 的变量名 + Qualifier 匹配另一个 Bean name 的变量名
+    // Case 16: Autowired + 匹配 Bean name 的变量名 + Qualifier 匹配另一个 Bean name 的变量名
     // 运行结果：正常
     // org.mazhuang.injecttest.service.impl.Callee1ServiceImpl
     // 通过 Qualifier 限定 Bean name 之后，变量名不再影响注入。
@@ -115,4 +122,15 @@ public class CallerServiceImpl implements CallerService {
 //    public void test() {
 //        System.out.println(callee2ServiceImpl.getClass().getName());
 //    }
+
+    // Case 17: Autowired + 注掉 CalleeService 的所有实现
+    // 运行结果：异常
+    // Exception encountered during context initialization - cancelling refresh attempt: org.springframework.beans.factory.UnsatisfiedDependencyException: Error creating bean with name 'callerServiceImpl': Unsatisfied dependency expressed through field 'calleeService'; nested exception is org.springframework.beans.factory.NoSuchBeanDefinitionException: No qualifying bean of type 'org.mazhuang.injecttest.service.CalleeService' available: expected at least 1 bean which qualifies as autowire candidate. Dependency annotations: {@org.springframework.beans.factory.annotation.Autowired(required=true)}
+//    @Autowired
+//    private CalleeService calleeService;
+
+    // Case 18: Autowired + 注掉 CalleeService 的所有实现 + required = false
+    // 运行结果：正常
+//    @Autowired(required = false)
+//    private CalleeService calleeService;
 }
